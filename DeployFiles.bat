e41ed92fb7e2e1c988217bec844dc2f10906e591
Marcus <marcus@xentro.se>
2021-12-05 19:02:10 +0100

    1. Upload

---
+++ DeployFiles.bat
@@ -0,0 +1,25 @@
+@echo off 
+
+:: Set path
+set substancePath="%userprofile%\Documents\Allegorithmic\Substance Painter\shelf\"
+
+echo Folder destination for our files are...
+echo %substancePath%
+echo:
+
+echo Moving files...
+@xcopy "shelf\" %substancePath% /S/Y/I
+
+echo:
+echo We are done here! 
+echo:
+
+pause
+
+:: /F = This option will display the full path and file name of both the source and destination files being copied.
+:: /I = Use the /i option to force xcopy to assume that destination is a directory.
+:: /R = Use this option to overwrite read-only files in destination. 
+:: /U = This option will only copy files in source that are already in destination.
+:: /Q = A kind of opposite of the /f option, the /q switch will put xcopy into "quiet" mode, skipping the on-screen display of each file being copied.
+:: /Y = Use this option to stop the xcopy command from prompting you about overwriting files from source that already exist in destination.
+:: /S = Copy folders and subfolders
---
+++ shelf/shaders/Giants Engine.glsl
@@ -0,0 +1,164 @@
+import lib-sss.glsl
+import lib-pbr.glsl
+import lib-emissive.glsl
+import lib-pom.glsl
+import lib-utils.glsl
+
+//: metadata {
+//:   "mdl":"mdl::alg::materials::physically_metallic_roughness::physically_metallic_roughness"
+//: }
+
+
+//-------- Functions ---------------------------------------------------//
+vec3 getBaseColorWithDefault(vec4 sampledValue, float defaultValue)
+{
+  return sampledValue.rgb + defaultValue * (1.0 - sampledValue.a);
+}
+vec3 getBaseColorWithDefault(SamplerSparse sampler, SparseCoord coord, float defaultValue)
+{
+  return getBaseColorWithDefault(textureSparse(sampler, coord), defaultValue);
+}
+
+float saturate(float value) {
+    return clamp(value, 0.0, 1.0);
+}
+float linearstepFS(float a, float b, float x) {
+    return saturate((x - a) / (b - a));
+}
+vec3 lerp(vec3 a, vec3 b, float w) {
+  return a + w * (b - a);
+}
+float lerp(float a, float b, float w) {
+  return a + w * (b - a);
+}
+
+
+//-------- Parameters ---------------------------------------------------//
+//: param custom { "default": true, "label": "Enable", "group": "Dirt Parameters" }
+uniform bool p_showDirt;
+//: param custom { "default": 1.0, "label": "Dirt Amount", "min": 0.0, "max": 1.0, "group": "Dirt Parameters" }
+uniform float p_dirtLevel;
+//: param custom { "default": [0.2, 0.14, 0.08], "label": "Dirt Color", "widget": "color", "group": "Dirt Parameters" }
+uniform vec3 p_dirtColor;
+
+//: param custom { "default": true, "label": "Enable", "group": "Wear Parameters" }
+uniform bool p_showWear;
+//: param custom { "default": 1.0, "label": "Wear Amount", "min": 0.0, "max": 1.0, "group": "Wear Parameters" }
+uniform float p_wearLevel;
+//: param custom { "default": [0.6, 0.6, 0.6], "label": "Wear Color", "widget": "color", "group": "Wear Parameters" }
+uniform vec3 p_wearColor;
+
+// Debug
+//: param custom { "default": false, "label": "Debug Mode", "group": "Debug" }
+uniform bool p_debugMode;
+//: param custom {
+//:   "default": 0,
+//:   "label": "Debug channel",
+//:   "widget": "combobox",
+//:   "group": "Debug",
+//:   "values": {
+//:     "BaseColor": 0,
+//:     "Roughness": 1,
+//:     "Metallic": 2,
+//:     "Dirt": 3,
+//:     "Wear": 4
+//:   }
+//: }
+uniform int p_debugChannel;
+//: param custom { "default": 0.7, "label": "Roughness Intensity", "min": 0.0, "max": 1.0, "group": "Debug" }
+uniform float p_debugRoughness;
+//: param custom { "default": 1.0, "label": "Metallic Intensity", "min": 0.0, "max": 1.0, "group": "Debug" }
+uniform float p_debugMetallic;
+
+
+//-------- Channels ---------------------------------------------------//
+//: param auto channel_basecolor
+uniform SamplerSparse basecolor_tex;
+//: param auto channel_roughness
+uniform SamplerSparse roughness_tex;
+//: param auto channel_metallic
+uniform SamplerSparse metallic_tex;
+//: param auto channel_specularlevel
+uniform SamplerSparse specularlevel_tex;
+
+//: param auto channel_user0
+uniform SamplerSparse dirt_tex;
+//: param auto channel_user1
+uniform SamplerSparse wear_tex;
+
+
+// Array data, Giants files needed here to go further
+vec3 gDetailDiffuse;
+vec3 gDetailSpecular  = vec3(0.1, 1.0, 0.0);
+vec3 gDetailNormal    = vec3(0.5, 0.5, 1.0);
+
+
+void shade(V2F inputs)
+{
+  float dirtLevel = 0;
+  float wearLevel = 0;
+
+  if (p_showDirt) { dirtLevel = p_dirtLevel; }
+  if (p_showWear) { wearLevel = p_wearLevel; }
+
+  // Apply parallax occlusion mapping if possible
+  vec3 viewTS = worldSpaceToTangentSpace(getEyeVec(inputs.position), inputs);
+  applyParallaxOffset(inputs, viewTS);
+
+  vec3 baseColor  = getBaseColor(basecolor_tex, inputs.sparse_coord);
+  vec3 wearMask   = getBaseColorWithDefault(wear_tex, inputs.sparse_coord, 0.0);
+  vec3 dirtMask   = getBaseColorWithDefault(dirt_tex, inputs.sparse_coord, 0.0);
+  float roughness = getRoughness(roughness_tex, inputs.sparse_coord);
+  float metallic  = getMetallic(metallic_tex, inputs.sparse_coord);
+
+  float mWear  = linearstepFS(1.0 - wearLevel, 1.0 - wearLevel + 0.05, wearMask.r);
+  float mDirt  = linearstepFS(1.0 - dirtLevel, 1.0 - dirtLevel + 0.5, dirtMask.r);
+  float gDirt  = saturate((dirtMask.r * dirtLevel) + mDirt);
+
+  metallic  = saturate(metallic + mWear) * p_debugMetallic;
+  metallic *= 1.0 - gDirt;
+  roughness = lerp(roughness, p_wearColor.r, mWear) * p_debugRoughness;
+  baseColor = lerp(baseColor, p_wearColor, mWear);
+
+  vec3 diffColor = generateDiffuseColor(baseColor, metallic);
+
+  float occlusion     = getAO(inputs.sparse_coord) * getShadowFactor();
+  float specOcclusion = specularOcclusionCorrection(occlusion, metallic, roughness);
+
+  diffColor *= gDetailSpecular.g;
+
+  vec3 mDirtDiffuse  = p_dirtColor * gDetailSpecular.g;
+  vec3 diffuseColor  = lerp(diffColor, mDirtDiffuse, gDirt);
+  vec3 specColorDirt = generateSpecularColor(1 - gDirt, baseColor, metallic);
+
+  if( !p_debugMode ) {
+    LocalVectors vectors = computeLocalFrame(inputs);
+
+    // Feed parameters for a physically based BRDF integration
+    emissiveColorOutput(pbrComputeEmissive(emissive_tex, inputs.sparse_coord));
+    albedoOutput(diffuseColor);
+    diffuseShadingOutput(occlusion * envIrradiance(vectors.normal));
+    specularShadingOutput(specOcclusion * pbrComputeSpecular(vectors, specColorDirt, roughness));
+    sssCoefficientsOutput(getSSSCoefficients(inputs.sparse_coord));
+  } else {
+    vec3 result;
+
+		if( p_debugChannel == 0 ) {
+			result = baseColor;
+		}
+		else if( p_debugChannel == 1 ) {
+			result = vec3(roughness);
+		}
+		else if( p_debugChannel == 2 ) {
+			result = vec3(metallic);
+		}
+    else if( p_debugChannel == 3 ) {
+			result = vec3(dirtMask.r, dirtMask.r, dirtMask.r);
+		}
+    else if( p_debugChannel == 4 ) {
+			result = vec3(wearMask.r, wearMask.r, wearMask.r);
+		}
+    
+    diffuseShadingOutput(result);
+  }
+}
